987f3d18f8f5b1f910117b9eb1264ef5162f08af
Gidia <cheongwh@me.com>
2015-07-11 20:43:28 +0900

    Marking, animation added

--- Queuing/app/src/main/java/com/example/owner/queuing/MainActivity.java
+++ Queuing/app/src/main/java/com/example/owner/queuing/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.owner.queuing;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.provider.Settings;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,22 +22,41 @@ import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 
 public class MainActivity extends FragmentActivity {
-    LinearLayout map;
+    LinearLayout mmap;
+    GoogleMap mGoogleMap;
+    LatLng loc = new LatLng(36.144425, 128.393269);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //LayoutInflater li = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        //FrameLayout layout_map = (FrameLayout)findViewById(R.id.layout_map);
-        //li.inflate(R.layout.activity_maps, layout_map, true);
 
-        map = (LinearLayout)findViewById(R.id.layout_map);
+        mmap = (LinearLayout) findViewById(R.id.layout_map);
         View child = getLayoutInflater().inflate(R.layout.activity_maps, null);
-        map.addView(child);
+        mmap.addView(child);
+        mGoogleMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+        AnimateCamera();
+        AddMarker();
+
     }
+
+    public void AnimateCamera(){
+        mGoogleMap.animateCamera(CameraUpdateFactory.newLatLng(loc));
+    }
+
+    public void AddMarker(){
+        mGoogleMap.addMarker(new MarkerOptions().position(new LatLng(36.144425, 128.393269)).title("KUMOKONGDAE").snippet("Maptest"));
+    }
+
 
 
 }
