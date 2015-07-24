c6cee10905da6a8102236f7e5a519664bc11c9fd
Cheong WoonHyoung <cheongwh@GidiaMacBook.local>
2015-07-23 17:35:34 -0700

    etc

--- Queuing/app/src/main/java/com/example/owner/queuing/RestaurantInfo.java
+++ Queuing/app/src/main/java/com/example/owner/queuing/RestaurantInfo.java
@@ -2,16 +2,35 @@ package com.example.owner.queuing;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
 
 /**
  * Created by owner on 2015-07-12.
  */
 public class RestaurantInfo extends Activity{
+
+    FrameLayout mFrame;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.restaurant_info);
 
+        mFrame = (FrameLayout)findViewById(R.id.res_back_btn);
+        mFrame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
     }
 }
