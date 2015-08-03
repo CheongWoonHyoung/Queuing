7987e573c152024b70c4bf125f6118342e7c8f57
mark_mac <wjyoon26@gmail.com>
2015-08-03 14:20:31 -0700

    upwardmenu_distance and picasso image added

--- Queuing/app/src/main/java/com/example/owner/queuing/ResListItem.java
+++ Queuing/app/src/main/java/com/example/owner/queuing/ResListItem.java
@@ -5,14 +5,14 @@ import android.graphics.Bitmap;
 /**
  * Created by mintaewon on 2015. 7. 24..
  */
-class ResListItem {
-    public Bitmap res_img;
+public class ResListItem {
+    public String small_imgurl;
     public String res_name;
     public String res_cuisine;
     public String res_distance;
 
-    public ResListItem(Bitmap img,String name, String cuisine, String distance){
-        this.res_img = img;
+    public ResListItem(String imgurl,String name, String cuisine, String distance){
+        this.small_imgurl = imgurl;
         this.res_name = name;
         this.res_cuisine = cuisine;
         this.res_distance = distance;
